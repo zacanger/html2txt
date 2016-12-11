2f797384c1fd3d9687f7819b7836cfb45d68576d
zacanger <zac@zacanger.com>
2016-12-10 22:02:40 -0700

    Handle https

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 
 const
-  url      = process.argv[2] || 'zacanger.com'
-, { get }  = require('http')
+  pkg      = require('./package.json')
+, url      = process.argv[2] || pkg.homepage
+, http     = require('http')
+, https    = require('https')
 , { rows } = process.stdout
 , len      = (rows < 80 ? 80 : rows)
 , toMd     = require('to-markdown')
@@ -12,11 +14,8 @@ const
 , conv     = a => toMd(a, opts)
 , wrap     = a => wrapper(a)
 , log      = a => console.log(a)
-, src      = url.includes('http://')
-  ? url
-  : url.includes('https://')
-    ? url.replace(/https/, 'http')
-    : `http://${url}`
+, src      = url.includes('://') ? url : `http://${url}`
+, get      = url.includes('https://') ? https.get : http.get
 
 const main = a => get(a, res => {
   let b = ''
